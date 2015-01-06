6d0d3bd822358a4b2908728fe42f1423d746673c
HeinzLeth <hlethaus@gmail.com>
2015-01-06 16:25:11 +0100

    edit

--- Chat.java
+++ Chat.java
@@ -31,29 +31,35 @@ public class Chat {
 
             int serverPort = 2534;
 
-            // BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Server ip eingeben:");
-            String serverIp = scanner.next();
+            BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+            //Scanner scanner = new Scanner(System.in);
 
-            //String ipServer = "";
-            //String ipClient = ipServer;
-            System.out.println("Port eingeben:");
-            boolean isPortInt = false;
+            Socket mySocket = null;
             int clientPort = 0;
-            while (!isPortInt) {
-                try {
-                    clientPort = scanner.nextInt();
-                    isPortInt = true;
-                } catch (InputMismatchException e) {
-                    System.out.println("Gib eine Zahl ein");
+            boolean isCorrectIp = false;
+            while (!isCorrectIp) {
+                System.out.println("Server ip eingeben:");
+                String serverIp = userIn.readLine();
+
+                //String ipServer = "";
+                //String ipClient = ipServer;
+                System.out.println("Port eingeben:");
+                boolean isPortInt = false;
+                while (!isPortInt) {
+                    try {
+                        clientPort = Integer.parseInt(userIn.readLine());
+                        isPortInt = true;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Gib eine Zahl ein");
+                    }
                 }
-            }
-            Socket mySocket = new Socket();
-            try {
-                mySocket.connect(new InetSocketAddress(serverIp, serverPort));
-            } catch (Exception e) {
-                System.out.println("Die IP-Adresse ist falsch");
+                try {
+                    mySocket = new Socket();
+                    mySocket.connect(new InetSocketAddress(serverIp, serverPort));
+                    isCorrectIp = true;
+                } catch (Exception e) {
+                    System.out.println("Die IP-Adresse ist falsch");
+                }
             }
             ServerSocket ssocket = new ServerSocket();
             ssocket.bind(new InetSocketAddress(clientPort));
@@ -63,34 +69,39 @@ public class Chat {
             String userInput = "";
 
             //String userName = "";
-            System.out.println("Please choose a username. If your input contains spaces the first will be your name!");
-            userInput = scanner.next();
             User user = new User();
+            boolean isConnected = false;
+            while (!isConnected) {
+                System.out.println("Please choose a username. NO spaces");
+                userInput = userIn.readLine();
 
-//            if (userInput.contains(" ")) {
-//                userInput = userInput.replaceAll(" ", "");
-//                System.out.println("Your spaces got removed automatically");
-//            }
-            user.setName(userInput);
-            user.setPort(clientPort);
+                if (userInput.contains(" ")) {
+                    userInput = userInput.replaceAll(" ", "");
+                    System.out.println("Your spaces got removed automatically");
+                }
 
-            out.write(String.format("%s %s %s\n", Commands.Client.login, userInput, clientPort));
-            out.flush();
-            String answer = in.readLine();
-            if (answer.equals(Commands.Server.loginSucessfull)) {
-                System.out.println("Hello " + userInput + "! You may now chat.");
-            } else if (answer.startsWith(Commands.Server.error)) {
-                System.out.println(answer.substring(2, answer.length()));
-                //schauen welche Fehlermeldung kommt
-            } else {
-                throw new RuntimeException("unknown answer from server");
+                user.setName(userInput);
+                user.setPort(clientPort);
+
+                out.write(String.format("%s %s %s\n", Commands.Client.login, userInput, clientPort));
+                out.flush();
+                String answer = in.readLine();
+                if (answer.equals(Commands.Server.loginSucessfull)) {
+                    System.out.println("Hello " + userInput + "! You may now chat.");
+                    isConnected = true;
+                } else if (answer.startsWith(Commands.Server.error)) {
+                    String error = answer.substring(2, answer.length());
+                    System.out.println(error);
+                } else {
+                    throw new RuntimeException("unknown answer from server");
+                }
             }
-
             ContactHandler contactHandler = new ContactHandler(ssocket, mySocket, in, out, user);
             contactHandler.start();
 
             UserInput input = new UserInput(contactHandler);
             input.start();
+
         } catch (IOException ex) {
             Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
         }
